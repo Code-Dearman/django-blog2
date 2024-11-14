cf792c8eac17153540801c98e7404619a497a0f1
Isaac Dearman <isaac.dearman@gmail.com>
2024-11-14 12:08:12 +0000

    practise debug

--- blog/models.py
+++ blog/models.py
@@ -36,7 +36,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    challange = models.SlugField()
+    challange = models.FloatField(default=3.0)
 
     class Meta:
         ordering = ["created_on"]
