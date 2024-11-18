60eb88d61425a68d2ca160108a52b9612e63c3b6
Isaac Dearman <isaac.dearman@gmail.com>
2024-11-18 11:58:45 +0000

    add about page

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-codedearman-djangoblog2-iffmsqqb9ih.ws.codeinstitute-ide.net',
+ALLOWED_HOSTS = ['8000-codedearman-djangoblog2-pcm4k0t8884.ws.codeinstitute-ide.net',
 '.herokuapp.com']
 
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'blog',
+    'about',
 ]
 
 MIDDLEWARE = [
